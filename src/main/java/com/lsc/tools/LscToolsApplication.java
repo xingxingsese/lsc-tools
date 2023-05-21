94617e9fc6b652c63bbd9ed9c1c86b1105a50fe1
lisc <zuihou098@qq.com>
2023-05-21 22:51:05 +0800

    springboot项目初始化

---
+++ src/main/java/com/lsc/tools/LscToolsApplication.java
@@ -0,0 +1,13 @@
+package com.lsc.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LscToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LscToolsApplication.class, args);
+    }
+
+}
